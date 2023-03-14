91a2188d9ea99795df6beb5f47412566e428be6a
sanjucs12 <sanjucs12@gmail.com>
2023-03-14 20:58:13 +0530

    Added delete button

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let expenselist=document.getElementById('expenselist')
+
 function savetolocal(event){
   event.preventDefault();
   const description=document.getElementById('description').value;
@@ -6,5 +8,24 @@ function savetolocal(event){
     description:description,
     amount:amount
   }
-  localStorage.setItem(description,JSON.stringify(obj))
+  localStorage.setItem(description,JSON.stringify(obj));
+
+  //CREATE LI ELEMENT
+ let li=document.createElement('li');
+ li.appendChild(document.createTextNode(description+amount))
+
+   //creating delete button
+   let deletebtn=document.createElement('button');
+   deletebtn.appendChild(document.createTextNode('Delete'))
+   deletebtn.onclick=()=>{
+    localStorage.removeItem(description);
+    expenselist.removeChild(li);
+   }
+   //adding delete button to li
+
+   li.appendChild(deletebtn)
+
+
+ //Append li element to list
+ expenselist.appendChild(li)
 }
