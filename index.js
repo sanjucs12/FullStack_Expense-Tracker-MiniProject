516b1ba0d3c8a4268f65e62c56abd9567d2f7c20
sanjucs12 <sanjucs12@gmail.com>
2023-03-23 00:35:36 +0530

    added bootstrap and styles

--- index.js
+++ index.js
@@ -9,23 +9,40 @@ function savetolocal(event){
     amount:amount
   }
   localStorage.setItem(description,JSON.stringify(obj));
+ display(obj)   
+}
+
+function display(obj){
 
   //CREATE LI ELEMENT
  let li=document.createElement('li');
- li.appendChild(document.createTextNode(description+amount))
+ li.appendChild(document.createTextNode(`${obj.description}------${obj.amount}`))
 
-   //creating delete button
-   let deletebtn=document.createElement('button');
-   deletebtn.appendChild(document.createTextNode('Delete'))
-   deletebtn.onclick=()=>{
-    localStorage.removeItem(description);
-    expenselist.removeChild(li);
-   }
-   //adding delete button to li
+//creating delete button
+let deletebtn=document.createElement('button');
+deletebtn.appendChild(document.createTextNode('Delete'))
+deletebtn.onclick=()=>{
+ localStorage.removeItem(obj.description);
+ expenselist.removeChild(li);
+}
 
-   li.appendChild(deletebtn)
+//adding delete button to li
+li.appendChild(deletebtn)
+
+//creating edit button
+let editbtn=document.createElement('button');
+editbtn.appendChild(document.createTextNode('Edit'))
+editbtn.onclick=()=>{
+  localStorage.removeItem(obj.description);
+  expenselist.removeChild(li);
+  document.getElementById('description').value=obj.description;
+  document.getElementById('amount').value=obj.amount;
+}
+
+//adding edit button to li
+li.appendChild(editbtn)
 
 
- //Append li element to list
- expenselist.appendChild(li)
+//Append li element to list
+expenselist.appendChild(li)
 }
